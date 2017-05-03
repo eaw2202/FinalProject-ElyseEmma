f182555d66f8fefb1a725feecd829e756939b0be
emlunder <eml2210@tc.columbia.edu>
2017-05-02 20:11:59 -0400

    Does not show questions from firebase yet...

--- app/src/main/java/com/elysewarren/elyseemma/QuestionScreen.java
+++ app/src/main/java/com/elysewarren/elyseemma/QuestionScreen.java
@@ -10,22 +10,29 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class QuestionScreen extends AppCompatActivity {
 
     private TextView displayText;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference arithmeticProblems = database.getReference("arithmetic");
     private DatabaseReference wpProblems = database.getReference("wordProblems");
+    private ArrayList<MathProblems> mathProblemsArrayList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
 
+        displayText = (TextView) findViewById(R.id.display_text);
+
         arithmeticProblems.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+                mathProblemsArrayList.add(dataSnapshot.getValue(MathProblems.class));
+                displayMathProblems();
             }
 
             @Override
@@ -75,6 +82,13 @@ public class QuestionScreen extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void displayMathProblems() {
+        String text = "";
+        for (MathProblems mathProblems : mathProblemsArrayList)
+            text += mathProblems + "\n";
+        displayText.setText(text);
     }
 
 }
